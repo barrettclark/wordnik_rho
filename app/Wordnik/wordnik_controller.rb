420344019f44fd2eb3e852086415adff06ac81d5
Barrett Clark <barrett@websitesbybarrett.com>
2010-05-18 15:14:05 -0500

    dictionary lookup functional

--- app/Wordnik/wordnik_controller.rb
+++ app/Wordnik/wordnik_controller.rb
@@ -6,20 +6,22 @@ class WordnikController < Rho::RhoController
   def index
     res = api_call("wordoftheday.json")
     @word = res['body']
-    render
   end
   def random
-    res = api_call("words.json/randomWord?hasDictionaryDef=true")
-    @word = res['body']
-    render
+    res1 = api_call("words.json/randomWord?hasDictionaryDef=true")
+    word = res1['body']
+    res2 = api_call("word.json/#{word['wordstring']}/definitions")
+    @definitions = res2['body']
+    puts "#{@definitions}"
   end
 
   def lookup    
   end
   def definition
-    res = definitions(@params['word'])
-    @word = res['body']
-    render
+    res = api_call("word.json/#{@params['word']}/definitions")
+    @definitions = res['body']
+    puts "#{@definitions}"
+    render :action => :random
   end
   
   protected
@@ -28,9 +30,5 @@ class WordnikController < Rho::RhoController
       :url => "http://api.wordnik.com/api/#{from}",
       :headers => {'api_key' => '93258932a2cf8104dce2604abe90708fa097c4d18b9a26e52'}
     )
-  end
-
-  def definitions(word)
-    api_call("word.json/#{word}/definitions")
   end
 end
