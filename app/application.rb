a476db799b0a7df04ec678b6cddc2d4e39949b96
Barrett Clark <barrett@websitesbybarrett.com>
2010-05-18 15:46:14 -0500

    trying icons from crystal clear

--- app/application.rb
+++ app/application.rb
@@ -3,12 +3,10 @@ require 'rho/rhoapplication'
 class AppApplication < Rho::RhoApplication
   def initialize
     @@toolbar = [
-      { :action => :back,    :icon => '/public/images/back_btn.png' },
-      { :action => :forward, :icon => '/public/images/forward_btn.png' },
-      { :action => :separator },
       { :action => :home },
-      { :action => '/app/Wordnik/random', :icon => '/public/images/whiteButton.png' },
-      { :action => '/app/Wordnik/lookup', :icon => '/public/images/grayButton.png' }
+      { :action => :separator },
+      { :action => '/app/Wordnik/random', :icon => '/public/images/crystal_clear_kblackbox.png' },
+      { :action => '/app/Wordnik/lookup', :icon => '/public/images/crystal_clear_find.png' }
     ]
     super
   end
