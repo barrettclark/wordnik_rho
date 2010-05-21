e8fc1c1e4fea83e424733727a4c217d80ccc4dd9
Barrett Clark <barrett@websitesbybarrett.com>
2010-05-20 22:54:31 -0500

    better toolbar icons

--- public/js/application.js
+++ public/js/application.js
@@ -1,1 +1,56 @@
-function helloWorld() {}
+$.jQTouch({
+            icon: 'jqtouch.png',
+            addGlossToIcon: false,
+            startupScreen: 'jqt_startup.png',
+            statusBar: 'black'
+        });
+
+        // Some sample Javascript functions:
+        $(function(){
+
+            // Show a swipe event on swipe test
+            $('#swipeme').addTouchHandlers().bind('swipe', function(evt, data){
+                $(this).html('You swiped <strong>' + data.direction + '</strong>!');
+                // alert('Swiped '+ data.direction +' on #' + $(evt.currentTarget).attr('id') + '.');
+            });
+
+            $('a[target="_blank"]').click(function(){
+                if(confirm('This link opens in a new window.'))
+                {
+                    return true;
+                }
+                else
+                {
+                    $(this).removeClass('active');
+                    return false;
+                }
+            });
+
+            // Page transition callback events
+            $('#pageevents').
+                bind('pageTransitionStart', function(e, info){
+                    $(this).find('.info').append('Started transitioning ' + info.direction + '&hellip; ');
+                }).
+                bind('pageTransitionEnd', function(e, info){
+                    $(this).find('.info').append(' finished transitioning ' + info.direction + '.<br /><br />');
+                });
+
+
+            // AJAX with callback event
+            $('#callback').
+                bind('pageTransitionEnd', function(e, info){
+                    if (info.direction == 'in' && $(this).data('loaded') != 'true')
+                    {
+                        $(this).
+                            append($('<div>Loading &hellip;</div>').
+                            load('ajax.html .info', function(){
+                                $(this).parent().data('loaded', 'true');
+                            }));
+                    }
+                });
+
+            // Orientation callback event
+            $('body').bind('turn', function(e, data){
+                $('#orient').html('Orientation: ' + data.orientation);
+            })
+        });
