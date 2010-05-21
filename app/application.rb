bd526b67e4d50fec27fef4cc5b348fa59dae2cff
Barrett Clark <barrett@websitesbybarrett.com>
2010-05-20 22:45:19 -0500

    better toolbar icons

--- app/application.rb
+++ app/application.rb
@@ -2,11 +2,12 @@ require 'rho/rhoapplication'
 
 class AppApplication < Rho::RhoApplication
   def initialize
+    # icons by Joseph Wain / glyphish.com
     @@toolbar = [
       { :action => :home },
       { :action => :separator },
-      { :action => '/app/Wordnik/random', :icon => '/public/images/crystal_clear_kblackbox.png' },
-      { :action => '/app/Wordnik/lookup', :icon => '/public/images/crystal_clear_find.png' }
+      { :action => '/app/Wordnik/random', :icon => '/public/images/05-shuffle.png' },
+      { :action => '/app/Wordnik/lookup', :icon => '/public/images/96-book.png' }
     ]
     super
   end
