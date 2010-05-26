90502f4516c3995c0abc1d5d59a3b842bbce1b6b
Barrett Clark <barrett@websitesbybarrett.com>
2010-05-25 21:25:08 -0500

    button down random word

--- app/Wordnik/wordnik_controller.rb
+++ app/Wordnik/wordnik_controller.rb
@@ -9,14 +9,25 @@ class WordnikController < Rho::RhoController
   end
   def random
     res1 = api_call("words.json/randomWord?hasDictionaryDef=true")
-    word = res1['body']
-    res2 = api_call("word.json/#{word['wordstring']}/definitions")
+    # puts "\n\n#{res1}\n\n"
+    @word = urlencode(res1['body']['wordstring'])
+    res2 = api_call("word.json/#{@word}/definitions")
     @definitions = res2['body']
-    puts "#{@definitions}"
+    # puts "\n\n>>> #{@definitions.class} #{@definitions} <<<\n\n"
+    if @definitions.class == String
+      render :action => 'unfound'
+    else
+      render
+    end
   end
 
-  def lookup    
+  def lookup
   end
+  def suggest
+  end
+  def unfound
+  end
+  
   def definition
     res = api_call("word.json/#{@params['word']}/definitions")
     @definitions = res['body']
@@ -28,7 +39,13 @@ class WordnikController < Rho::RhoController
   def api_call(from)
     Rho::AsyncHttp.get(
       :url => "http://api.wordnik.com/api/#{from}",
-      :headers => {'api_key' => 'my_api_key'}
+      :headers => {'api_key' => '93258932a2cf8104dce2604abe90708fa097c4d18b9a26e52'}
     )
+  end
+
+  def urlencode(str)
+    str.gsub!(/ /, '+')
+    str.gsub!(/'/, '%27')
+    str
   end
 end
